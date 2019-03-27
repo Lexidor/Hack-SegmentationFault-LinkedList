c9d2fa25d9c1f3dbbe43ea7cd04a358b1c4ca127
Lexidor Digital <31805625+Lexidor@users.noreply.github.com>
2019-03-27 22:09:42 +0100

    Remove a `\n`

--- lexidor/Test.hh
+++ lexidor/Test.hh
@@ -5,5 +5,5 @@ function main_test(): void {
     require_once __DIR__.'/../vendor/autoload.hack';
     Facebook\AutoloadMap\initialize();
     $list = new LinkedList(range(0, 1000000));
-    echo "\nAnd now the garbase collector...\n";
+    echo "And now the garbage collector...\n";
 }
