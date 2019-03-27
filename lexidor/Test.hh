75127f28f2fca9f50a1ce2f380385d879bfc7fbb
Lexidor Digital <31805625+Lexidor@users.noreply.github.com>
2019-03-27 21:55:11 +0100

    Update to hhvm 4.1

--- lexidor/Test.hh
+++ lexidor/Test.hh
@@ -2,7 +2,8 @@
 
 <<__EntryPoint>>
 function main_test(): void {
-    require_once __DIR__.'/../vendor/hh_autoload.hh';
+    require_once __DIR__.'/../vendor/autoload.hack';
+    Facebook\AutoloadMap\initialize();
     $list = new LinkedList(range(0, 1000000));
     $list->__toString();
     foreach ($list->toGenerator() as $key => $val) {
---
+++ src/Tags.hh
@@ -0,0 +1,5 @@
+<?hh // strict
+
+class TimeComplexity implements HH\MethodAttribute {
+    public function __construct(string $_) {}
+}
