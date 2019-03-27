59f8bc380c8b5c8142ae0a1ca714b0de8a134ce5
Lexidor Digital <31805625+Lexidor@users.noreply.github.com>
2019-03-27 22:08:19 +0100

    Simplify case

--- lexidor/Test.hh
+++ lexidor/Test.hh
@@ -5,8 +5,5 @@ function main_test(): void {
     require_once __DIR__.'/../vendor/autoload.hack';
     Facebook\AutoloadMap\initialize();
     $list = new LinkedList(range(0, 1000000));
-    $list->__toString();
-    foreach ($list->toGenerator() as $key => $val) {
-        printf("(%d, %d)\n", $key, $val);
-    }
+    echo "\nAnd now the garbase collector...\n";
 }
